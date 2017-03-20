76386dbb1ed7083112eb6dcff8647ec83c8fab65
Hao Luo <howlowck@gmail.com>
2017-03-19 21:44:33 -0500

    removed Web.config, and moved server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const project = require('../config/project.config')
-const server = require('../server/main')
+const project = require('./config/project.config')
+const server = require('./server/main')
 const debug = require('debug')('app:bin:dev-server')
 
 server.listen(project.server_port)

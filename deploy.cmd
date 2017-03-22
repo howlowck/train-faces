8017b137c0e78780d96b604b2a0fedd9c669a2ff
Hao Luo <howlowck@gmail.com>
2017-03-22 13:34:42 -0500

    changed deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install :: removed --production in order to build static assets
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )

e1d5ba9e4df0b819437115163cbf71433e21b086
Hao Luo <howlowck@gmail.com>
2017-03-27 17:32:48 -0500

    fixed linting error

--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -13,7 +13,11 @@ export const HomePage = ({ guests }) => (
     <h1>Get Ready to Train your Face API!!!</h1>
     <h2>How to use this tool:</h2>
     <ol>
-      <li>1. Get a key from the <a href='https://www.microsoft.com/cognitive-services/en-us/face-api'>Cognitive Service Face API <Icon type='export' /></a></li>
+      <li>1. Get a key from the
+        <a href='https://www.microsoft.com/cognitive-services/en-us/face-api'>
+          Cognitive Service Face API <Icon type='export' />
+        </a>
+      </li>
       <li>2. Copy and paste the key into "FaceApiKey" in the <Link to='config'>Configure</Link> Section</li>
       <li>3. <Link to='person-groups/_create'>Create a PersonGroup</Link></li>
       <li>4. Go to your created group and Add a Person (like you).</li>
